290383c1428da73ed207a83ebd217e4b7a282a56
sifat02 <syedsifat02@gmail.com>
2022-06-08 16:46:19 +0600

    ..

---
+++ resources/views/administrator/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>{{ __('Version') }}</b> 7.1.2
+    </div>
+    <strong>{{ __('Copyright') }} &copy; 2019 <a href="https://amazingsoftbd.com" target="_blank">{{ __('Amazing Soft') }}</a>.</strong> {{ __('All rights reserved. - shared on    c o d e l i s t . c c') }}
+</footer>
